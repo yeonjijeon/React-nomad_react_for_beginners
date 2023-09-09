9ae2fc335a4fce1b7f1a16f9e1931bc60b5ca79d
YeonJi <jeonyeonji1028@gmail.com>
2023-09-09 22:09:35 +0900

    09/09 map()을 이용한 to do list

--- react-for-beginers/src/App.js
+++ react-for-beginers/src/App.js
@@ -1,54 +1,39 @@
-import Button from "./Button";
-import styles from './App.module.css'
-import { useEffect, useState } from "react";
-
-function Hello() {
-  function destroyedFn () {
-    console.log('destroyed :(')
-  }
-  
-  function effectFn () {
-    console.log("created :)")
-    return destroyedFn // clean up fuction
-  }
-  
-  useEffect(effectFn, [])
-  return <h1>hello</h1>
-}
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [counter, setValue] = useState(0)
-  const [keyword, setKeyword] = useState('')
-  const [showing, setShowing] = useState(false)
-  const onClick = () => setValue((prev) => prev + 1)
-  const onChange = (event) => {
-    setKeyword(event.target.value)
-  }
-  const onClickShow = () => setShowing((prev) => !prev)
-
-  console.log('i run all the time')
-  
-  useEffect(() => {
-    console.log("CALL THE API...")
-  }, [])
-
-  useEffect(() => {
-    if(keyword !== '' && keyword.length > 5) {
-      console.log('SEARCH FOR ', keyword)
+  const [todo, setTodo] = useState('')
+  const [todos, setTodos] = useState([])
+  const onChange = (event) => setTodo(event.target.value)
+  const onSubmit = (event) => {
+    event.preventDefault()
+    if (todo === '') {
+      return
     }
-  }, [keyword])
+    setTodos((currentArray) => [todo, ...currentArray])
+    setTodo('')
+  }
+  console.log(todos)
 
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."/>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click Me</button>
-      <div>
-        {showing ? <Hello/> : null}
-        <button onClick={onClickShow}>{showing ? 'Hide' : 'Show'}</button>  
-      </div>
+      <h1>My To Dos ({todos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={todo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {todos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
