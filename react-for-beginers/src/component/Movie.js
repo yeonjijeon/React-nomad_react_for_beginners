b7fc83a54a5696780d1d1068a091ba55b853964c
YeonJi <jeonyeonji1028@gmail.com>
2023-09-11 13:46:54 +0900

    09/10 propTypes 수정

--- react-for-beginers/src/component/Movie.js
+++ react-for-beginers/src/component/Movie.js
@@ -17,12 +17,12 @@ function Movie({ id, coverImg, title, summary, genres }) {
     )
 }
 
-// Movie.propTypes = {
-//   id: PropTypes.number.isReauired,
-//   coverImg: PropTypes.string.isReauired,
-//   title: PropTypes.string.isReauired,
-//   summary: PropTypes.string.isReauired,
-//   genres: PropTypes.arrayOf(PropTypes.string).isReauired,
-// }
+Movie.propTypes = {
+    id: PropTypes.number.isRequired,
+    coverImg: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+}
 
 export default Movie
